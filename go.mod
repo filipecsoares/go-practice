39cc2cb23ffd3cfe9c55d920d0300327b27bdaa7
filipecsoares <filipecarlossoares@gmail.com>
2023-05-21 08:00:44 -0300

    feat: urlshort from yaml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/filipecsoares/go-practice
 
 go 1.20
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -19,4 +21,51 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		}
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(data []byte) ([]pathUrl, error) {
+	var parsedYaml []pathUrl
+	err := yaml.Unmarshal(data, &parsedYaml)
+	if err != nil {
+		return nil, err
+	}
+	return parsedYaml, nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
