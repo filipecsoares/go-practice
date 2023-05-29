4268f8760b1700058ace62e24b179b91fef83dd7
filipecsoares <filipecarlossoares@gmail.com>
2023-05-29 07:56:11 -0300

    algorithms: fibonacci

--- algorithms/fibonacci/fibonacci.go
+++ algorithms/fibonacci/fibonacci.go
@@ -1,8 +1,23 @@
 package fibonacci
 
 func NthFibonacci(num int) int {
-    if num == 0 || num == 1 {
-        return num
-    }
-    return NthFibonacci(num - 1) + NthFibonacci(num - 2)
+	if num == 0 || num == 1 {
+		return num
+	}
+	return NthFibonacci(num-1) + NthFibonacci(num-2)
+}
+
+func IsFibonacci(num uint) bool {
+	if num <= 1 {
+		return true
+	}
+	var first uint = 0
+	var second uint = 1
+	for i := 0; i < int(num); i++ {
+		first, second = second, first+second
+		if num == uint(second) {
+			return true
+		}
+	}
+	return false
 }
--- algorithms/fibonacci/fibonacci_test.go
+++ algorithms/fibonacci/fibonacci_test.go
@@ -3,34 +3,62 @@ package fibonacci
 import "testing"
 
 func TestNthFibonacci(t *testing.T) {
-    expected := 1
-    result := NthFibonacci(1)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
-    expected = 1
-    result = NthFibonacci(2)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
-    expected = 2
-    result = NthFibonacci(3)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
-    expected = 3
-    result = NthFibonacci(4)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
-    expected = 5
-    result = NthFibonacci(5)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
-    expected = 8
-    result = NthFibonacci(6)
-    if expected != result {
-        t.Errorf("Expect: %d, Got: %d", expected, result)
-    }
+	expected := 1
+	result := NthFibonacci(1)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+	expected = 1
+	result = NthFibonacci(2)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+	expected = 2
+	result = NthFibonacci(3)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+	expected = 3
+	result = NthFibonacci(4)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+	expected = 5
+	result = NthFibonacci(5)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+	expected = 8
+	result = NthFibonacci(6)
+	if expected != result {
+		t.Errorf("Expect: %d, Got: %d", expected, result)
+	}
+}
+
+func TestIsFibonacci(t *testing.T) {
+	expected := true
+	result := IsFibonacci(1)
+	if expected != result {
+		t.Errorf("Expect: %t, Got: %t", expected, result)
+	}
+	expected = true
+	result = IsFibonacci(2)
+	if expected != result {
+		t.Errorf("Expect: %t, Got: %t", expected, result)
+	}
+	expected = true
+	result = IsFibonacci(3)
+	if expected != result {
+		t.Errorf("Expect: %t, Got: %t", expected, result)
+	}
+	expected = false
+	result = IsFibonacci(4)
+	if expected != result {
+		t.Errorf("Expect: %t, Got: %t", expected, result)
+	}
+	expected = true
+	result = IsFibonacci(5)
+	if expected != result {
+		t.Errorf("Expect: %t, Got: %t", expected, result)
+	}
 }
