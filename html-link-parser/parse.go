b702b2ad03f0d25ca18e813e24368629833bab13
filipecsoares <filipecarlossoares@gmail.com>
2023-05-31 08:50:16 -0300

    link: map tags

--- html-link-parser/parse.go
+++ html-link-parser/parse.go
@@ -1,7 +1,10 @@
 package htmllinkparser
 
 import (
+	"fmt"
 	"io"
+
+	"golang.org/x/net/html"
 )
 
 // Link represents a link (<a href="...">) in an HTML document.
@@ -12,5 +15,21 @@ type Link struct {
 
 // Parse will take in an HTML document and will return a slice of links parsed.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	linkNodes(doc, "")
 	return nil, nil
+}
+
+func linkNodes(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		linkNodes(n, padding+" ")
+	}
 }
