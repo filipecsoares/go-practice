e4fe6903331f358d4629f5789fa8a8913800db43
filipecsoares <filipecarlossoares@gmail.com>
2023-06-17 10:41:58 -0300

    algorithms: base to decimal

---
+++ algorithms/calhoun_algorithms/module_01/base_to_dec.go
@@ -0,0 +1,29 @@
+package module01
+
+// BaseToDec takes in a number and the base it is currently
+// in and returns the decimal equivalent as an integer.
+//
+// Eg:
+//
+//	BaseToDec("E", 16) => 14
+//	BaseToDec("1110", 2) => 14
+func BaseToDec(value string, base int) int {
+	const charset = "0123456789ABCDEF"
+	res := 0
+	multiplier := 1
+	for i := len(value) - 1; i >= 0; i-- {
+		index := -1
+		for j, char := range charset {
+			if char == rune(value[i]) {
+				index = j
+				break
+			}
+		}
+		if index < 0 {
+			panic("Something went wrong!")
+		}
+		res = res + index*multiplier
+		multiplier *= base
+	}
+	return res
+}
